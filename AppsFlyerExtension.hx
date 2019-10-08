8acdb98e9d71f484422f1d2528676957fcf69a71
Марат Исмагилов <myflashworks@gmail.com>
2019-10-08 18:36:43 +0300

    update

--- AppsFlyerExtension.hx
+++ AppsFlyerExtension.hx
@@ -42,12 +42,12 @@ class AppsFlyerExtension {
 	
 	public static function startTracking (devKey:String, appId:String = ""):Void {
 		#if (android)
-		var fn = JNI.createStaticMethod(
-			"org.haxe.extension.AppsFlyerExtension",
-			"init",
-			"(Ljava/lang/String;)V"
-		);
-		JNI.callStatic(fn, [devKey]);
+//		var fn = JNI.createStaticMethod(
+//			"org.haxe.extension.AppsFlyerExtension",
+//			"init",
+//			"(Ljava/lang/String;)V"
+//		);
+//		JNI.callStatic(fn, [devKey]);
 		#end
 
 		#if (ios)
