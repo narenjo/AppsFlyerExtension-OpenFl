96351aec5eaa2f5af40598a88216ebfab373637b
Марат Исмагилов <myflashworks@gmail.com>
2019-12-29 18:05:40 +0200

    fix errors after sync

--- project/include/AppsFlyerAppInterface.h
+++ project/include/AppsFlyerAppInterface.h
@@ -3,7 +3,6 @@
 #include <string>
 
 namespace appsflyerextension {
-	void Pre_init();
 	void StartTracking(std::string devkey, std::string appId);
 	void TrackEvent(std::string eventName, std::string eventData);
 	void Init();
